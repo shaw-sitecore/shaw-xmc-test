0502df4326d85f3854418f24b971da14a5ea6120
Shawn Pan <shaw@sitecore.net>
2025-09-18 12:11:08 +0800

    Update PDFContainer

--- headapps/nextjs-starter/src/components/TestSite/PDFContainer.tsx
+++ headapps/nextjs-starter/src/components/TestSite/PDFContainer.tsx
@@ -1,17 +1,19 @@
 import { ComponentProps } from '@sitecore-feaas/clientside/react';
 import { File, FileField } from '@sitecore-jss/sitecore-jss-nextjs';
 
+interface Fields {
+  File: FileField;
+}
 type PDFContainerProps = ComponentProps & {
-  fields: {
-    file: FileField;
-  };
+  params: { [key: string]: string };
+  fields: Fields;
 };
 
 export default function PDFContainer(props: PDFContainerProps) {
   return (
     <div>
-      <File field={props.fields.file} target="_blank">
-        <h1>{props.fields.file.value.title}</h1>
+      <File field={props.fields.File} target="_blank">
+        <h1>{props.fields.File.value.title}</h1>
       </File>
     </div>
   );
