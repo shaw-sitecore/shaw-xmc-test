3a4505c0eeecd070ec3926578f45ec7eb7a2c987
Shawn Pan <shaw@sitecore.net>
2025-09-18 13:15:35 +0800

    Move comments

--- headapps/nextjs-starter/src/components/TestSite/PDFContainer.tsx
+++ headapps/nextjs-starter/src/components/TestSite/PDFContainer.tsx
@@ -11,11 +11,9 @@ type PDFContainerProps = ComponentProps & {
 
 export default function PDFContainer(props: PDFContainerProps) {
   return (
-    <div>
-      // target="_blank" opens the file on a new tab. Remove to open the file on the current tab
-      <File field={props.fields.PDF}>
-        <h1>{props.fields.PDF.value.src}</h1>
-      </File>
-    </div>
+    // target="_blank" opens the file on a new tab. Remove to open the file on the current tab
+    <File field={props.fields.PDF}>
+      <h1>{props.fields.PDF.value.src}</h1>
+    </File>
   );
 }
