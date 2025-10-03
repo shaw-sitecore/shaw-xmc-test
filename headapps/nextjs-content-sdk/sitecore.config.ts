01650be341ff79ba5f5299c7a704747eb76c220f
Shawn Pan <shaw@sitecore.net>
2025-10-03 17:39:30 +0800

    Revert sitecore config mods

--- headapps/nextjs-content-sdk/sitecore.config.ts
+++ headapps/nextjs-content-sdk/sitecore.config.ts
@@ -5,32 +5,4 @@ import { defineConfig } from '@sitecore-content-sdk/nextjs/config';
  * See the documentation for `defineConfig`:
  * https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
  */
-export default defineConfig({
-  generateStaticPaths: !process.env.GENERATE_STATIC_PATHS,
-  api: {
-    edge: {
-      contextId:
-        process.env.SITECORE_EDGE_CONTEXT_ID ||
-        process.env.NEXT_PUBLIC_SITECORE_EDGE_CONTEXT_ID ||
-        '',
-      clientContextId: process.env.NEXT_PUBLIC_SITECORE_EDGE_CONTEXT_ID,
-      edgeUrl: process.env.SITECORE_EDGE_URL || process.env.NEXT_PUBLIC_SITECORE_EDGE_URL,
-    },
-  },
-  defaultSite: process.env.SITECORE_EDITING_SECRET,
-  defaultLanguage: process.env.NEXT_PUBLIC_DEFAULT_LANGUAGE || 'en',
-  editingSecret: process.env.SITECORE_EDITING_SECRET,
-  redirects: {
-    enabled: false,
-    locales: ['en'],
-  },
-  multisite: {
-    enabled: false,
-    useCookieResolution: () => process.env.VERCEL_ENV === 'preview',
-  },
-  personalize: {
-    scope: process.env.NEXT_PUBLIC_PERSONALIZE_SCOPE,
-    edgeTimeout: parseInt(process.env.PERSONALIZE_MIDDLEWARE_EDGE_TIMEOUT!, 10),
-    cdpTimeout: parseInt(process.env.PERSONALIZE_MIDDLEWARE_EDGE_TIMEOUT!, 10),
-  },
-});
+export default defineConfig({});
