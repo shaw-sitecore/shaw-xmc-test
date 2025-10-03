4237a666bea89d251c4e06f6ac7736ddeb33bb14
Shawn Pan <shaw@sitecore.net>
2025-10-03 17:50:58 +0800

    Remove some custom sitecore config env vars

--- headapps/nextjs-content-sdk/sitecore.config.ts
+++ headapps/nextjs-content-sdk/sitecore.config.ts
@@ -5,4 +5,29 @@ import { defineConfig } from '@sitecore-content-sdk/nextjs/config';
  * See the documentation for `defineConfig`:
  * https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
  */
-export default defineConfig({});
+export default defineConfig({
+  api: {
+    edge: {
+      contextId:
+        process.env.SITECORE_EDGE_CONTEXT_ID ||
+        process.env.NEXT_PUBLIC_SITECORE_EDGE_CONTEXT_ID ||
+        '',
+      clientContextId: process.env.NEXT_PUBLIC_SITECORE_EDGE_CONTEXT_ID,
+      edgeUrl: process.env.SITECORE_EDGE_URL || process.env.NEXT_PUBLIC_SITECORE_EDGE_URL,
+    },
+  },
+  defaultLanguage: process.env.NEXT_PUBLIC_DEFAULT_LANGUAGE || 'en',
+  redirects: {
+    enabled: false,
+    locales: ['en'],
+  },
+  multisite: {
+    enabled: false,
+    useCookieResolution: () => process.env.VERCEL_ENV === 'preview',
+  },
+  personalize: {
+    scope: process.env.NEXT_PUBLIC_PERSONALIZE_SCOPE,
+    edgeTimeout: parseInt(process.env.PERSONALIZE_MIDDLEWARE_EDGE_TIMEOUT!, 10),
+    cdpTimeout: parseInt(process.env.PERSONALIZE_MIDDLEWARE_EDGE_TIMEOUT!, 10),
+  },
+});
