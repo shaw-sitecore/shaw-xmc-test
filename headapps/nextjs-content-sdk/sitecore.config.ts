19e3017730ce57fb20791228f02f004a801ba00f
Shawn Pan <shaw@sitecore.net>
2025-10-03 18:02:54 +0800

    AGAIN!

--- headapps/nextjs-content-sdk/sitecore.config.ts
+++ headapps/nextjs-content-sdk/sitecore.config.ts
@@ -6,16 +6,6 @@ import { defineConfig } from '@sitecore-content-sdk/nextjs/config';
  * https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
  */
 export default defineConfig({
-  api: {
-    edge: {
-      contextId:
-        process.env.SITECORE_EDGE_CONTEXT_ID ||
-        process.env.NEXT_PUBLIC_SITECORE_EDGE_CONTEXT_ID ||
-        '',
-      clientContextId: process.env.NEXT_PUBLIC_SITECORE_EDGE_CONTEXT_ID,
-      edgeUrl: process.env.SITECORE_EDGE_URL || process.env.NEXT_PUBLIC_SITECORE_EDGE_URL,
-    },
-  },
   defaultLanguage: process.env.NEXT_PUBLIC_DEFAULT_LANGUAGE || 'en',
   redirects: {
     enabled: false,
